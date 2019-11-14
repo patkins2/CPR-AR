ad3300d3b1d2ca2264908b013f2703e3826f03bf
Paul Atkins <patkins2@uncc.edu>
2019-11-14 16:07:34 -0500

    Cleaning up a bit

--- Assets/Scripts/MyUIManager.cs
+++ Assets/Scripts/MyUIManager.cs
@@ -2,30 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyUIManager : MonoBehaviour {
+public class MyUIManager : MonoBehaviour
+{
 
     [SerializeField] GameObject buttonConnect, button130;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void ShowAllButtons()
     {
         buttonConnect.SetActive(true);
-        button130.SetActive(true);
+        //button130.SetActive(true);
     }
 
     public void HideAllButtons()
     {
         buttonConnect.SetActive(false);
-        button130.SetActive(false);
+        //button130.SetActive(false);
     }
 
 }
