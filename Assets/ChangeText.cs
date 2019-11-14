2e6864a48f702d75b829680e106874c2d23fa594
Paul Atkins <patkins2@uncc.edu>
2019-11-14 16:02:16 -0500

    Initial commit

---
+++ Assets/ChangeText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    public Text displayText;
+
+    void Start()
+    {
+        //Text sets your text to say this message
+        //displayText.text = "Press Ok to Begin";
+    }
+
+    void Update()
+    {
+        //Press the space key to change the Text message
+        if (Input.GetKey(KeyCode.Space))
+        {
+            displayText.text = "My text has now changed.";
+        }
+    }
+
+    public void Test()
+    {
+        displayText.text = "test";
+    }
+    
+   
+}
---
+++ Assets/MyUIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyUIManager : MonoBehaviour {
+
+    [SerializeField] GameObject buttonConnect, button130;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowAllButtons()
+    {
+        buttonConnect.SetActive(true);
+        button130.SetActive(true);
+    }
+
+    public void HideAllButtons()
+    {
+        buttonConnect.SetActive(false);
+        button130.SetActive(false);
+    }
+
+}
