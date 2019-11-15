98a4c294e3e961f7f711fde84340909843994ff7
Paul Atkins <patkins2@uncc.edu>
2019-11-15 15:00:06 -0500

    Following Panel Added
    Panel now follows the camera. But still needs a few tweaks plus the actual instructions.

--- Assets/Scripts/ChangeText.cs
+++ Assets/Scripts/ChangeText.cs
@@ -16,14 +16,17 @@ public class ChangeText : MonoBehaviour
     void Update()
     {
         //Press the space key to change the Text message
-        if (Input.GetKey(KeyCode.Space))
-        {
-            displayText.text = "My text has now changed.";
-        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    displayText.text = "My text has now changed.";
+        //}
     }
 
     public void Test()
     {
+        //displayText = GetComponent<TextMesh>();
+        //TextMesh t = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
+        //t.text = "test";
         displayText.text = "test";
     }
     
