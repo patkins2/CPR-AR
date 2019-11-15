7b488e4f580f478137b2c5f8ad7b22557b5f2c64
Paul Atkins <patkins2@uncc.edu>
2019-11-15 15:07:17 -0500

    Revert "Following Panel Added"
    This reverts commit 98a4c294e3e961f7f711fde84340909843994ff7.

--- Assets/Scripts/ChangeText.cs
+++ Assets/Scripts/ChangeText.cs
@@ -16,17 +16,14 @@ public class ChangeText : MonoBehaviour
     void Update()
     {
         //Press the space key to change the Text message
-        //if (Input.GetKey(KeyCode.Space))
-        //{
-        //    displayText.text = "My text has now changed.";
-        //}
+        if (Input.GetKey(KeyCode.Space))
+        {
+            displayText.text = "My text has now changed.";
+        }
     }
 
     public void Test()
     {
-        //displayText = GetComponent<TextMesh>();
-        //TextMesh t = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
-        //t.text = "test";
         displayText.text = "test";
     }
     
