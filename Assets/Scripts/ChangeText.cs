439b84c2520448c1cd770cac7902605c4c880d7f
Paul Atkins <patkins2@uncc.edu>
2019-11-20 13:51:27 -0500

    Changing the panel added
    Panel can now change multiple times. Just need to add the instructions and adjust the size of the panel and button.

--- Assets/Scripts/ChangeText.cs
+++ Assets/Scripts/ChangeText.cs
@@ -5,30 +5,48 @@ using UnityEngine.UI;
 
 public class ChangeText : MonoBehaviour
 {
-    public Text displayText;
+    public Text displayText; //the text that is displayed
+    public int counter; //counter for screen navigation
 
     void Start()
     {
         //Text sets your text to say this message
         //displayText.text = "Press Ok to Begin";
+        counter = 1;
     }
 
     void Update()
     {
         //Press the space key to change the Text message
-        //if (Input.GetKey(KeyCode.Space))
-        //{
-        //    displayText.text = "My text has now changed.";
-        //}
+        /*if (Input.GetKey(KeyCode.Space))
+        {
+            displayText.text = "My text has now changed.";
+        }*/
     }
 
     public void Test()
     {
-        //displayText = GetComponent<TextMesh>();
-        //TextMesh t = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
-        //t.text = "test";
-        displayText.text = "test";
-    }
-    
-   
+        //the value of counter will determine which screen is displayed. More can be added, just create more cases.
+        switch (counter)
+        {
+            case 1:
+                displayText.text = "1st Screen";
+                counter++;
+                break;
+            case 2:
+                displayText.text = "2nd Screen";
+                counter++;
+                break;
+            case 3:
+                displayText.text = "3rd Screen";
+                counter++;
+                break;
+
+            default:
+                displayText.text = "Press OK to begin"; //assuming you've been through the whole menu, counter will reset to 1
+                counter = 1;
+                break;
+        }
+        //displayText.text = "test";
+    } 
 }
